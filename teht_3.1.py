096d8ca2b6061013d27ed9f813c1bae15ce43ede
Andy2518 <andykl@metropolia.fi>
2026-08-18 15:06:04 +0300

    3 moduulin ensimmäinen tehtävä

---
+++ teht_3.1.py
@@ -0,0 +1,6 @@
+kuha = float(input("Kuinka pitkä kuha on?"))
+
+if kuha <= 37:
+    alamittainen = 37 - kuha
+    print("Läske kuha takaisin järveen.")
+    print(f">Kalasi on {alamittainen} cm liian lyhyt.")
