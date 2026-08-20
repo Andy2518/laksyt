fe89d9bb4850a465f1400bcf9ee76cf4c062d5d4
Andy2518 <andykl@metropolia.fi>
2026-08-20 20:51:12 +0300

    2 moduulin kuudennes tehtävä

---
+++ teht_2.6.py
@@ -0,0 +1,16 @@
+import random
+
+numeromerkki1 = random.randint(0,9)
+numeromerkki2 = random.randint(0,9)
+numeromerkki3 = random.randint(0,9)
+
+print(f"kolmenumeroinen koodi: {numeromerkki1}{numeromerkki2}{numeromerkki3}")
+
+numero1 = random.randint(1,6)
+numero2 = random.randint(1,6)
+numero3 = random.randint(1,6)
+numero4 = random.randint(1,6)
+
+print(f"nelinumeroinen koodi: {numero1}{numero2}{numero3}{numero4}")
+
+
