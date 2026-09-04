4a9d9b585d1eb72b4602da9da33b2cb424366e6d
Andy2518 <andykl@metropolia.fi>
2026-09-04 10:28:12 +0300

    6 moduuli kuudennes tehtävä (muokattu)

--- 6 moduuli/teht_6.6.py
+++ 6 moduuli/teht_6.6.py
@@ -1,11 +1,10 @@
 import math
 
-def pizza (halkaisija, euro):
-    sade = halkaisija / 2
-    metri = sade / 100
-    pinta_ala = math.pi * (metri**2)
-    euro / pinta_ala
-    return euro
+def pizza (halkaisija, hinta):
+    metri = halkaisija / 100
+    sade = metri / 2
+    pinta_ala = math.pi * sade * sade
+    return hinta / pinta_ala
 
 pizza1_halkaisija = int(input("Anna ensimmäisen pizzan halkaisija:"))
 pizza1_hinta = float(input("Anna ensimmäisen pizzan hinta:"))
@@ -16,6 +15,6 @@ pizza1 = pizza(pizza1_halkaisija, pizza1_hinta)
 pizza2 = pizza(pizza2_halkaisija, pizza2_hinta)
 
 if pizza1 > pizza2:
-    print("Pizza1 antaa paremman vastineen rahalle.")
-elif pizza1 < pizza2:
     print("Pizza2 antaa paremman vastineen rahalle.")
+elif pizza1 < pizza2:
+    print("Pizza1 antaa paremman vastineen rahalle.")
